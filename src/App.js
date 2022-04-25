706184de3355cabfc1a06fe2dd4b00c34955af96
Anu Lama <anulama@u.boisestate.edu>
2022-04-25 11:55:01 -0600

    init button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AuraButton from './components/Button/AuraButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AuraButton variant='contained'> A button</AuraButton>
+      <AuraButton variant='outlined'> Outlined </AuraButton>
     </div>
   );
 }
