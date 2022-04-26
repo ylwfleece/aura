fbf877f5aee4045d11b4da71c3b6747bb1f73339
Anu Lama <anulama@u.boisestate.edu>
2022-04-25 18:04:23 -0600

    update styling

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,30 +1,72 @@
-import "./LoginForm.css";
-import AuraButton from "../Button/AuraButton";
-import Input from "../Input/Input";
-import Error from "../Error/Error.js";
-import { Button } from "@mui/material";
+import './LoginForm.css';
+import AuraButton from '../Button/AuraButton';
+import Input from '../Input/Input';
+import Error from '../Error/Error.js';
+import { Button } from '@mui/material';
+import headerSvg from '../../assets/svg/aura-logo-icon.svg';
+import gmailIcon from '../../assets/images/gmail_icon.png';
 // background-image: url("./assets/images/shutterstock_363952133.jpg");
 // ./assets/images/fav-icon.jpg
 
+const GmailIcon = () => {
+  return <img style={{ height: '20px', width: '20px' }} src={gmailIcon} />;
+};
+
 const LoginForm = (props) => {
   return (
-    <div className="jumbo-card">
-      <div className="jumbo-card-top">
-        <img src='../../assets/images/pdf_icon.png' className="aura-logo-img" />
-        <div className="aura-logo-txt">Aura</div>
+    // <div className='jumbo-card'>
+    //   <div className='jumbo-card-top'>
+    //     <img src={headerSvg} className='aura-logo-img' />
+    //   </div>
+    //   <div className='login-form-container'>
+    //     <section className='login-standard'>
+    //       <Input placeholder='Email Address' type='email' />
+
+    //       <Input placeholder='Password' type='password' />
+    //       {/* <Error /> */}
+
+    //       <AuraButton>login</AuraButton>
+    //       <span className='forgot-password'>forgot pw</span>
+    //     </section>
+    //     {/* <span className='login-hr'>___________</span> */}
+    //     <hr className='horizontal-line' />
+    //     <section className='login-google'>
+    //       <span className='login-using'>login using</span>
+    //       <AuraButton>google</AuraButton>
+    //     </section>
+    //   </div>
+    // </div>
+    <div className='jumbo-card'>
+      <div className='jumbo-card-top'>
+        <img src={headerSvg} className='aura-logo-img' />
       </div>
-      <div className="login-form-container">
-        <section className="login-standard">
-          <Input placeholder="Email Address" type="email" />
-          <Input placeholder="Password" type="password"/>
-          {/* <Error /> */}
-          <AuraButton>login</AuraButton>
-          <span className="forgot-password">forgot pw</span>
+      <div className='login-form-container'>
+        <section className='login-standard'>
+          <div className='form-row'>
+            <Input placeholder='Email Address' type='email' fullWidth={true} />
+          </div>
+          <div className='form-row'>
+            <Input placeholder='Password' type='password' fullWidth={true} />
+          </div>
+
+          <div className='btn-container'>
+            <AuraButton variant='contained' fullWidth={true}>
+              Login
+            </AuraButton>
+            <span className='forgot-password'>Forgot Password?</span>
+          </div>
         </section>
-        <span className="login-hr">___________</span>
-        <section className="login-google">
-          <span className="login-using">login using</span>
-          <AuraButton>google</AuraButton>
+        {/* <span className='login-hr'>___________</span> */}
+        <hr className='horizontal-line' />
+        <section className='login-google'>
+          <span className='login-using'>Login using</span>
+          <AuraButton
+            startIcon={<GmailIcon />}
+            sx={{ textTransform: 'none' }}
+            color='error'
+          >
+            Gmail
+          </AuraButton>
         </section>
       </div>
     </div>
